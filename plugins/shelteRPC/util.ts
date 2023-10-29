0cde8261ccbb4a61dd12b414b8eab30c3497f16c
SpikeHD <spikegdofficial@gmail.com>
2023-10-29 02:13:42 -0700

    fix: add utils

---
+++ plugins/shelteRPC/util.ts
@@ -0,0 +1,12 @@
+const baseUrl = 'https://cdn.discordapp.com'
+
+export const generateIconUrl = (appId: string, icon: string) => {
+  return `${baseUrl}/app-icons/${appId}/${icon}.png`
+}
+
+export const generateCoverUrl = (appId: string, cover: string) => {
+  return `${baseUrl}/app-assets/${appId}/store/${cover}.png`
+}
+export const generateAssetUrl = (appId: string, asset: string) => {
+  return `${baseUrl}/app-assets/${appId}/${asset}.png`
+}
