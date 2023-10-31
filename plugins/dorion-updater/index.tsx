8a65bbe738c9fc067ec865cb279bcb365880f8a4
SpikeHD <spikegdofficial@gmail.com>
2023-10-31 13:47:41 -0700

    fix: updater not destroying everything on Windows

--- plugins/dorion-updater/index.tsx
+++ plugins/dorion-updater/index.tsx
@@ -1,10 +1,41 @@
+interface ConfirmationModalProps {
+  header: string
+  body: string
+  confirmText: string
+  cancelText?: string
+  onConfirm?: () => void
+  onCancel?: () => void
+  type?: 'neutral' | 'danger' | 'warning'
+}
+
 const {
-  ui: { openConfirmationModal },
+  ui: {
+    openModal,
+    ModalRoot,
+    ModalHeader,
+    ModalBody,
+    ModalConfirmFooter
+  },
 } = shelter
 
 const { invoke, process, event } = (window as any).__TAURI__
+const confirmModal = (props: ConfirmationModalProps) => (
+  <ModalRoot>
+    <ModalHeader>{props.header}</ModalHeader>
+    <ModalBody>{props.body}</ModalBody>
+    <ModalConfirmFooter
+      onConfirm={props.onConfirm}
+      onCancel={props.onCancel}
+      close={props.onCancel}
+      confirmText={props.confirmText}
+      cancelText={props.cancelText}
+      type={props.type}
+    />
+  </ModalRoot>
+)
 
-export const onLoad = async () => {
+const load = async () => {
+  console.log('[Updater] Checking for updates...')
   const config = JSON.parse(await invoke('read_config_file'))
   // This returns an array of what to update, if anything.
   const updateCheck = await invoke('update_check')
@@ -24,15 +55,14 @@ export const onLoad = async () => {
     if (config.autoupdate) {
       // We should still warn that Dorion is going to restart
       if (updateCheck.includes('dorion')) {
-        openConfirmationModal({
+        openModal((props) => confirmModal({
           header: 'Dorion Update',
           body: 'A Dorion update has been fetched, and Dorion will restart momentarily.',
           confirmText: 'Got it!',
           type: 'neutral',
-        }).then(
-          () => doUpdate(),
-          () => {},
-        )
+          onConfirm: () => doUpdate(),
+          onCancel: props.close,
+        }))
         return
       }
 
@@ -40,30 +70,30 @@ export const onLoad = async () => {
       return
     }
 
-    openConfirmationModal({
+    openModal((props) => confirmModal({
       header: 'Updates Available!',
       body: 'There are Dorion updates available. Would you like to apply them? This notification can be disabled in Dorion Settings',
       confirmText: 'Yes please!',
       cancelText: 'Nope!',
       type: 'neutral',
-    }).then(
-      () => doUpdate(),
-      () => {},
-    )
+      onConfirm: () => doUpdate(),
+      onCancel: props.close,
+    }))
   }
 
   // Listen for update_complete eevent
-  event.listen('update_complete', () => {
-    openConfirmationModal({
+  event.once('update_complete', () => {
+    openModal((props) => confirmModal({
       header: 'Update Complete!',
       body: 'The update has been applied! Please restart to apply the changes.',
       confirmText: 'Okay!',
       type: 'neutral',
-    }).then(
-      () => process.relaunch(),
-      () => {},
-    )
+      onConfirm: () => process.relaunch(),
+      onCancel: props.close,
+    }))
   })
 }
 
 export const onUnload = () => {}
+
+load()
