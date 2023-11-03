a57f072e4475fe2f26728568495681c6f5da62eb
SpikeHD <spikegdofficial@gmail.com>
2023-11-02 23:19:47 -0700

    feat(dorion-settings): add dorion version to bottom area

--- plugins/dorion-settings/index.tsx
+++ plugins/dorion-settings/index.tsx
@@ -5,8 +5,32 @@ import { SettingsPage } from './components/SettingsPage'
 const {
   settings: {
     registerSection,
+  },
+  flux: {
+    dispatcher
+  },
+  util: {
+    sleep
   }
 } = shelter
+
+const { app } = (window as any).__TAURI__
+
+const appendDorionVersion = async () => {
+  await sleep(1000)
+
+  const versionThings = document.querySelector('div[class*="side_"] div[class*="info_"]')
+  const firstChild = versionThings.firstElementChild as HTMLSpanElement
+  const newVersionThing = document.createElement('span') as HTMLSpanElement
+
+  newVersionThing.innerHTML = `Dorion v${await app.getVersion()}`
+  newVersionThing.classList.add(...firstChild.classList)
+  newVersionThing.style.color = firstChild.style.color
+
+  versionThings.appendChild(newVersionThing)
+}
+
+dispatcher.subscribe('USER_SETTINGS_MODAL_OPEN', appendDorionVersion)
 
 const settingsUninjects = [
   registerSection('divider'),
@@ -18,4 +42,5 @@ const settingsUninjects = [
 
 export const onUnload = () => {
   settingsUninjects.forEach((u) => u())
+  dispatcher.unsubscribe('USER_SETTINGS_MODAL_OPEN', appendDorionVersion)
 }
