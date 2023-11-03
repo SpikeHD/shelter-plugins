c8f33d88a77fda6674715d5e390f00366293fb21
SpikeHD <spikegdofficial@gmail.com>
2023-11-03 00:00:07 -0700

    feat(blur-nsfw): create blur-nsfw plugin

---
+++ plugins/blur-nsfw/index.ts
@@ -0,0 +1,62 @@
+const {
+  flux: {
+    dispatcher,
+    stores: {
+      ChannelStore
+    }
+  },
+} = shelter
+
+let injectedCss = false
+let tempStyle = null
+
+const handleNsfwChannelSelect = async (payload) => {
+  const { channelId } = payload
+  const channel = ChannelStore.getChannel(channelId)
+
+  if (!channel.nsfw_) {
+    if (tempStyle) {
+      tempStyle.remove()
+      injectedCss = false
+    }
+
+    return
+  }
+
+  if (injectedCss) return
+
+  const style = document.createElement('style')
+  style.innerText = `
+    div[class*="imageWrapper_"] video,
+    div[class*="imageWrapper_"] img {
+      filter: blur(10px) !important;
+
+      transition: filter 0.5s ease;
+    }
+
+    /* On hover, show the image */
+    div[class*="imageWrapper_"]:hover video,
+    div[class*="imageWrapper_"]:hover img {
+      filter: blur(0) !important;
+    }
+
+    /* If the user clicked it, the don't need it blurred anymore */
+    div[class*="focusLock_"] video,
+    div[class*="focusLock_"] img {
+      filter: blur(0) !important;
+    }
+  `
+
+  tempStyle = document.body.appendChild(style)
+}
+
+dispatcher.subscribe('CHANNEL_SELECT', handleNsfwChannelSelect)
+
+export const onUnload = () => {
+  dispatcher.unsubscribe('CHANNEL_SELECT', handleNsfwChannelSelect)
+
+  if (tempStyle) {
+    tempStyle.remove()
+    injectedCss = false
+  }
+}
