07842c37470cfd12e69805dedfe4f83b57383fbf
SpikeHD <spikegdofficial@gmail.com>
2023-11-03 14:53:55 -0700

    feat(invisible-typing): toggle on message bar

---
+++ plugins/invisible-typing/index.tsx
@@ -0,0 +1,71 @@
+import { css, classes } from './index.scss'
+
+const {
+  flux: {
+    intercept
+  },
+  plugin: {
+    store
+  },
+  solid: {
+    createSignal
+  },
+  ui: {
+    injectCss,
+    tooltip
+  },
+  observeDom
+} = shelter
+
+false && tooltip
+
+let injectedCss = false
+
+if (!injectedCss) {
+  injectedCss = true
+  injectCss(css)
+}
+
+const keyboardSvg = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"> 
+    { /* Font Awesome Free 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. */ }
+    <path fill="currentColor" d="M64 64C28.7 64 0 92.7 0 128V384c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H64zm16 64h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm80-176c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V144zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zM160 336c0-8.8 7.2-16 16-16H400c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V336zM272 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM256 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM368 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM352 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V240zM464 128h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16zM448 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V240zm16 80h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H464c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16z"/>
+  </svg>
+)
+
+const unintercept = intercept(dispatch => {
+  if (dispatch.type === 'TYPING_START_LOCAL') return store.enabled ? false : null
+})
+
+const unobserve = observeDom('[class^="channelTextArea"] [class^="buttons"]', (node) => {
+  if (document.querySelector('#invis-icon')) return
+
+  const [enabled, setEnabled] = createSignal(!!store.enabled)
+
+  const toggleEnabled = () => {
+    store.enabled = !enabled()
+    setEnabled(!enabled())
+  }
+
+  const invisIcon = (
+    <div
+      id="invis-icon"
+      class={classes.invisContainer + (enabled() ? ' ' + classes.notShowing : '')}
+      onClick={toggleEnabled}
+      use:tooltip={enabled() ? 'Currently hiding' : 'Currently not hiding'}
+    >
+      {keyboardSvg}
+    </div>
+  )
+
+  node.prepend(invisIcon)
+})
+
+export const onUnload = () => {
+  unintercept()
+  unobserve()
+
+  // Get rid of invis icon if it's still around
+  const invisIcon = document.querySelector('#invis-icon')
+  if (invisIcon) invisIcon.remove()
+}
