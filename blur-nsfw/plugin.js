e96247485be975b796bb0823c6c8cec96c7725ae
SpikeHD <SpikeHD@users.noreply.github.com>
2023-11-10 03:58:59 +0000

    deploy: dd44c72136e8db818c6af24b0bc25f96ccc49d27

--- blur-nsfw/plugin.js
+++ blur-nsfw/plugin.js
@@ -1,4 +1,4 @@
-(()=>{var c=Object.defineProperty;var v=Object.getOwnPropertyDescriptor;var b=Object.getOwnPropertyNames;var g=Object.prototype.hasOwnProperty;var C=(t,e)=>{for(var n in e)c(t,n,{get:e[n],enumerable:!0})},_=(t,e,n,r)=>{if(e&&typeof e=="object"||typeof e=="function")for(let s of b(e))!g.call(t,s)&&s!==n&&c(t,s,{get:()=>e[s],enumerable:!(r=v(e,s))||r.enumerable});return t};var E=t=>_(c({},"__esModule",{value:!0}),t);var m=(t,e,n)=>new Promise((r,s)=>{var p=i=>{try{a(n.next(i))}catch(o){s(o)}},u=i=>{try{a(n.throw(i))}catch(o){s(o)}},a=i=>i.done?r(i.value):Promise.resolve(i.value).then(p,u);a((n=n.apply(t,e)).next())});var N={};C(N,{onUnload:()=>L});var{flux:{dispatcher:f,stores:{ChannelStore:y}}}=shelter,d=!1,l=null,h=t=>m(void 0,null,function*(){let{channelId:e}=t;if(!y.getChannel(e).nsfw_){l&&(l.remove(),d=!1);return}if(d)return;let r=document.createElement("style");r.innerText=`
+(()=>{var c=Object.defineProperty;var b=Object.getOwnPropertyDescriptor;var g=Object.getOwnPropertyNames;var _=Object.prototype.hasOwnProperty;var C=(s,e)=>{for(var t in e)c(s,t,{get:e[t],enumerable:!0})},E=(s,e,t,n)=>{if(e&&typeof e=="object"||typeof e=="function")for(let i of g(e))!_.call(s,i)&&i!==t&&c(s,i,{get:()=>e[i],enumerable:!(n=b(e,i))||n.enumerable});return s};var y=s=>E(c({},"__esModule",{value:!0}),s);var f=(s,e,t)=>new Promise((n,i)=>{var h=r=>{try{a(t.next(r))}catch(o){i(o)}},v=r=>{try{a(t.throw(r))}catch(o){i(o)}},a=r=>r.done?n(r.value):Promise.resolve(r.value).then(h,v);a((t=t.apply(s,e)).next())});var N={};C(N,{onUnload:()=>L});var{flux:{dispatcher:p,stores:{ChannelStore:d}}}=shelter,m=!1,l=null,u=s=>f(void 0,null,function*(){let{channelId:e}=s;if(!(d==null?void 0:d.getChannel(e)).nsfw_){l&&(l.remove(),m=!1);return}if(m)return;let n=document.createElement("style");n.innerText=`
     div[class*="imageWrapper_"] video,
     div[class*="imageWrapper_"] img {
       filter: blur(10px) !important;
@@ -17,4 +17,4 @@
     div[class*="focusLock_"] img {
       filter: blur(0) !important;
     }
-  `,l=document.body.appendChild(r)});f.subscribe("CHANNEL_SELECT",h);var L=()=>{f.unsubscribe("CHANNEL_SELECT",h),l&&(l.remove(),d=!1)};return E(N);})();
+  `,l=document.body.appendChild(n)});p.subscribe("CHANNEL_SELECT",u);var L=()=>{p.unsubscribe("CHANNEL_SELECT",u),l&&(l.remove(),m=!1)};return y(N);})();
