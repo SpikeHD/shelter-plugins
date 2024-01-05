3f582be8f9fa8d8991a6bb2528cc281dfe386a27
Luximus <thomaslipman10@gmail.com>
2024-01-05 12:27:48 +0100

    fix(dorion-changelog): a lot (see description)
    - fetch changelog on client, not rust
    - create proper release types
    - don't initially load changelog from local storage
    - add loading screen
    - move changelog related functions to changelog util
    - add parallel processing for images and processing release bodies

--- plugins/dorion-settings/components/ChangelogPage.tsx
+++ plugins/dorion-settings/components/ChangelogPage.tsx
@@ -1,18 +1,25 @@
+import type { IRelease, TReleases } from '../types/release.ts'
 import { css, classes } from './ChangelogPage.tsx.scss'
-import { marked } from 'marked'
+import { processReleaseBodies, loadChangelog, fixImageLinks } from '../util/changelog.js'
+
+const PAGE_ID = 'dorion-changelog-tab'
 
 const {
-  ui: { injectCss, Header, HeaderTags, showToast, Button, ButtonSizes, ButtonColors, Text, LinkButton },
+  ui: { 
+    injectCss, 
+    Header, 
+    HeaderTags, 
+    Button, 
+    ButtonSizes, 
+    ButtonColors, 
+    Text, 
+    LinkButton 
+  },
   solid: { createSignal, createEffect },
 } = shelter
 
 const { invoke, app } = (window as any).__TAURI__
 let injectedCss = false
-
-interface IRelease {
-  version: string;
-  changes: string;
-}
 
 export function ChangelogPage() {
   if (!injectedCss) {
@@ -21,16 +28,14 @@ export function ChangelogPage() {
   }
 
   const [loading ,setLoading] = createSignal<boolean>(true)
-  const [releases, setReleases] = createSignal<IRelease[]>([])
+  const [releases, setReleases] = createSignal<TReleases>([])
   const [currentVersion, setCurrentVersion] = createSignal<string>('')
   const [latestVersion, setLatestVersion] = createSignal<string>('')
-  const [needsUpdate, setNeedsUpdate] = createSignal<boolean>(false)
-
-  setReleases(loadChangelogFromLocalStorage())
+  const [updateCheck, setUpdateCheck] = createSignal<string[]>([])
 
   createEffect(async () => {
     // Load changelog from GitHub
-    setReleases(await loadChangelogFromGitHub())
+    setReleases(await processReleaseBodies(await loadChangelog()))
     
     // Set current version
     setCurrentVersion(`v${await app.getVersion()}`)
@@ -41,151 +46,65 @@ export function ChangelogPage() {
     }
 
     // Check for updates
-    const updateCheck = await invoke('update_check')
-    if (updateCheck.includes('dorion')) {
-      setNeedsUpdate(true)
-    }
+    setUpdateCheck(await invoke('update_check'))
 
-    // Fix image links
-    await fixImageLinks()
-
-    // Done loading
+    // Done loading except for images
+    // Show page as soon as possible to prevent loading page for a long time
     setLoading(false)
+    
+    // Fix image links
+    await fixImageLinks(document.getElementById(PAGE_ID))
   }, [])
 
-  async function doUpdate () {
-    const updateCheck = await invoke('update_check')
+  async function doUpdate() {
     invoke('do_update', {
-      toUpdate: updateCheck,
+      toUpdate: updateCheck()
     })
-  }
-  
-  function sanitizeChangelog(releases: IRelease[]): IRelease[] {
-    console.log('Sanitizing changelog')
-
-    if (releases == null || releases.length < 1)
-      throw new Error('Changelog is null. You probably ran out of GitHub API requests. Try again later.')
-
-    const temp: IRelease[] = []
-    releases.forEach((release) => {
-      const version = release.version
-      const changes = release.changes
-        .replace(/@([\w-]+)/g, '<a href="https://github.com/$1">@$1</a>') // GitHub user
-        .replace(/ #(\d+)/g, ' <a href="https://github.com/spikehd/dorion/pull/$1">#$1</a>') // GitHub issue or PR
-        .replace(/<a href="([^"]+)">([^<]+)<\/a>/g, '<a href="$1" target="_blank">$2</a>') // External link
-      temp.push({ version, changes })
-    })
-    return temp
-  }
-
-  async function fetchChangelogFromGitHub(): Promise<IRelease[] | null> {
-    const changelog: string[] | null = await invoke('get_changelog')
-
-    if (changelog == null || changelog.length < 1)
-      throw new Error('Changelog is null. You probably ran out of GitHub API requests. Try again later.')
-
-    const temp: IRelease[] = []
-    await changelog.forEach(async (entry) => {
-      const lines = entry.split('\n')
-      const version = lines.shift().replace('# ', '')
-      let changes = await marked.parse(lines.join('\n'))
-      changes = changes.replace('\n', '')
-
-      const release = {
-        version,
-        changes,
-      } 
-
-      temp.push(release)
-    })
-
-    return (temp.length > 1) ? temp : null
-  }
-
-  async function loadChangelogFromGitHub(): Promise<IRelease[]> {
-    let changelog: IRelease[] = []
-
-    try {
-      changelog = sanitizeChangelog(await fetchChangelogFromGitHub())
-
-      if (changelog == null || changelog.length < 1)
-        throw new Error('Changelog is null. You probably ran out of GitHub API requests. Try again later.')
-
-      saveChangelogToLocalStorage(changelog)
-    }
-    catch (e) {
-      console.error(e)
-      showToast({
-        title: 'Failed to load changelog',
-        content: e.message,
-        duration: 3000,
-      })
-
-      changelog = loadChangelogFromLocalStorage()
-    }
-
-    return changelog
-  }
-
-  function loadChangelogFromLocalStorage(): IRelease[] {
-    const changelog = localStorage.getItem('changelog')
-    if (!changelog) return []
-    return JSON.parse(changelog)
-  }
-
-  function saveChangelogToLocalStorage(changelog: IRelease[]): void {
-    localStorage.setItem('changelog', JSON.stringify(changelog))
-  }
-
-  async function fixImageLinks(): Promise<void> {
-    const page = document.getElementById('dorion-changelog-tab')
-    if (!page) return
-
-    const images = page.getElementsByTagName('img')
-
-    for (let i = 0; i < images.length; i++) {
-      const image = images[i]
-      const url = image.src
-      const response = await window.fetch(url)
-      image.src = await response.image()
-    }
   }
 
   async function refresh(): Promise<void> {
     setLoading(true)
-    setReleases(await loadChangelogFromGitHub())
-    await fixImageLinks()
+    setReleases(await processReleaseBodies(await loadChangelog()))
     setLoading(false)
+    await fixImageLinks(document.getElementById(PAGE_ID))
   }
 
   return (
     <>
       <Header tag={HeaderTags.H1} class={classes.tophead}>Changelog</Header>
       <Button onClick={refresh} disabled={loading()} class={classes.refresh}>Refresh</Button>
-      {needsUpdate() && (
+      {loading() ? (
         <div class={classes.card}>
-          <Header tag={HeaderTags.H1} class={classes.title}>Update available!</Header>
-          <Text>Your current version is {currentVersion()}</Text>
-          <Button size={ButtonSizes.LARGE} color={ButtonColors.GREEN} onClick={doUpdate}>Update to {latestVersion()}</Button>
+          <div class={classes.spinner} />
         </div>
-      )}
-      {releases().map((release: IRelease) => (
-        <div class={classes.card}>
-          <Header tag={HeaderTags.H1} class={classes.title}>
-            <span>
-              {release.version}
-            </span>
-            <div class={classes.badges}>
-              {currentVersion() == release.version && 
-                <span class={classes.badge}>Current</span>}
-              {releases()[0].version == release.version && 
-                <span class={classes.badge}>Latest</span>}
+      ) : (
+        <>  
+          {updateCheck().includes('dorion') && (
+            <div class={classes.card}>
+              <Header tag={HeaderTags.H1} class={classes.title}>Update available!</Header>
+              <Text>Your current version is {currentVersion()}</Text>
+              <Button size={ButtonSizes.LARGE} color={ButtonColors.GREEN} onClick={doUpdate}>Update to {latestVersion()}</Button>
             </div>
-          </Header>
-          <LinkButton href={`https://github.com/SpikeHD/Dorion/releases/tag/${release.version}`}>View on GitHub</LinkButton>
-          <div class={classes.contents} innerHTML={release.changes} />
-        </div>
-      ))}
+          )}
+          {releases() != null && releases().length > 0 && releases().map((release: IRelease) => (
+            <div class={classes.card}>
+              <Header tag={HeaderTags.H1} class={classes.title}>
+                <span>
+                  {release.name}
+                </span>
+                <div class={classes.badges}>
+                  {currentVersion() == release.tag_name && 
+                    <span class={classes.badge}>Current</span>}
+                  {releases()[0].version == release.tag_name && 
+                    <span class={classes.badge}>Latest</span>}
+                </div>
+              </Header>
+              <LinkButton href={release.html_url}>View on GitHub</LinkButton>
+              <div class={classes.contents} innerHTML={release.body} />
+            </div>
+          ))}
+        </>
+      )}
     </>
   )
 }
---
+++ plugins/dorion-settings/util/changelog.ts
@@ -0,0 +1,88 @@
+import { marked } from 'marked'
+import { TReleases } from '../types/release.js'
+
+const {
+  ui: { showToast },
+  plugins: { installedPlugins }
+} = shelter
+
+const devModeReservedId = '__DEVMODE_PLUGIN_DO_NOT_USE_OR_YOU_WILL_BE_FIRED'
+
+function isDevMode(): boolean {
+  return installedPlugins() && devModeReservedId in installedPlugins()
+}
+
+export async function loadChangelog(): Promise<TReleases> {
+  ///NOTE - This is a thing for development. Otherwise GitHub will rate limit us :)
+  if (isDevMode()) {
+    console.warn('[Dorion Changelog] Dev mode is on. Loading changelog from local storage.')
+    return _loadChangelogFromLocalStorage()
+  }
+
+  try {
+    const changelog = await _fetchChangelogFromGitHub()
+    _saveChangelogToLocalStorage(changelog)
+    return changelog
+  }
+  catch (e) {
+    console.error(e)
+    showToast({
+      title: 'Failed to load changelog',
+      content: e.message,
+      duration: 3000,
+    })
+
+    return _loadChangelogFromLocalStorage()
+  }
+}
+
+async function _fetchChangelogFromGitHub(): Promise<TReleases> {
+  const response = await fetch('https://api.github.com/repos/SpikeHD/Dorion/releases')
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch changelog. ${response.status} ${response.statusText}`)
+  }
+
+  return await response.json() as TReleases
+}
+
+function _loadChangelogFromLocalStorage(): TReleases {
+  const changelog = localStorage.getItem('changelog')
+  if (!changelog) return []
+  return JSON.parse(changelog)
+}
+
+function _saveChangelogToLocalStorage(changelog: TReleases): void {
+  localStorage.setItem('changelog', JSON.stringify(changelog))
+}
+
+export async function processReleaseBodies(releases: TReleases): Promise<TReleases> {
+  const sanitizedReleases = await Promise.all(releases.map(async (release) => {
+    release.body = await _processReleaseBody(release.body)
+    return release
+  }))
+
+  return sanitizedReleases
+}
+
+export async function _processReleaseBody(body: string): Promise<string> {
+  const parsedBody = await marked.parse(body)
+
+  return parsedBody
+    .replace('\n', '') // remove newlines. It's converted to html, so it's not needed
+    .replace(/@([\w-]+)/g, '<a href="https://github.com/$1">@$1</a>') // GitHub user
+    .replace(/ #(\d+)/g, ' <a href="https://github.com/spikehd/dorion/pull/$1">#$1</a>') // GitHub issue or PR
+    .replace(/<a href="([^"]+)">([^<]+)<\/a>/g, '<a href="$1" target="_blank">$2</a>') // External link
+}
+
+export async function fixImageLinks(scope: HTMLElement): Promise<void> {
+  if (!scope) return
+
+  const images = scope.getElementsByTagName('img')
+
+  await Promise.all(Array.from(images).map(async (image: HTMLImageElement) => {
+    const url = image.src
+    const response = await window.fetch(url)
+    image.src = await response.image()
+  }))    
+}
