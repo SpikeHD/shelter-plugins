155d31d7e27b57c423bb0b294cb764c564d0e947
SpikeHD <spikegdofficial@gmail.com>
2024-01-11 15:38:28 -0800

    fix(all): lints

--- css.d.ts
+++ css.d.ts
@@ -1,14 +1,14 @@
-declare module "*.css" {
-  export const css: string;
-  export const classes: Record<string, string>;
+declare module '*.css' {
+  export const css: string
+  export const classes: Record<string, string>
 }
 
-declare module "*.scss" {
-  export const css: string;
-  export const classes: Record<string, string>;
+declare module '*.scss' {
+  export const css: string
+  export const classes: Record<string, string>
 }
 
-declare module "*.sass" {
-  export const css: string;
-  export const classes: Record<string, string>;
+declare module '*.sass' {
+  export const css: string
+  export const classes: Record<string, string>
 }
