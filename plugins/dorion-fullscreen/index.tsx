e0f9c688bca9f57e6fa5ef87880b59124ff26df9
SpikeHD <spikegdofficial@gmail.com>
2024-01-11 20:09:05 -0800

    fix(dorion-fullscreen): hide topbar

--- plugins/dorion-fullscreen/index.tsx
+++ plugins/dorion-fullscreen/index.tsx
@@ -11,6 +11,12 @@ const {
 } = (window as any).__TAURI__
 
 const toggleFullscreen = async (payload) => {
+  const topbar = document.querySelector('#dorion_topbar') as HTMLDivElement
+
+  if (topbar) {
+    topbar.style.display = payload?.isElementFullscreen ? 'none' : 'initial'
+  }
+
   appWindow?.setFullscreen(payload?.isElementFullscreen)
 }
 
