4e24f72271b0f3178a1fe65a1e95da48a48c75ef
SpikeHD <spikegdofficial@gmail.com>
2024-07-09 21:07:06 -0700

    feat: prep dorion plugins for v2

--- api/dorion.ts
+++ api/dorion.ts
@@ -2,7 +2,12 @@ export default {
   name: 'Dorion',
 
   invoke: (name: string, payload?: any) => {
-    return (window as any).__TAURI__.invoke(name, payload)
+    // for tauri v2, `invoke` exists on __TAURI__.core, instead of base __TAURI__, so we should check for both
+    if ((window as any).__TAURI__?.invoke) {
+      return (window as any).__TAURI__.invoke(name, payload)
+    } else {
+      return (window as any).__TAURI__.core.invoke(name, payload)
+    }
   },
   event: {
     emit: (name: string, payload: any) => {
@@ -25,7 +30,12 @@ export default {
   apiWindow: {
     appWindow: {
       setFullscreen: (isFullscreen: boolean) => {
-        return (window as any).__TAURI__.window.appWindow.setFullscreen(isFullscreen)
+        // for tauri v2, `setFullscreen` exists on __TAURI__.webviewWindow.getCurrent()[0], instead of base __TAURI__.window.appWindow, so we should check for both
+        if ((window as any).__TAURI__?.webviewWindow?.getCurrent) {
+          return (window as any).__TAURI__.webviewWindow.getCurrent()[0].setFullscreen(isFullscreen)
+        } else {
+          return (window as any).__TAURI__.window.appWindow.setFullscreen(isFullscreen)
+        }
       }
     }
   }
