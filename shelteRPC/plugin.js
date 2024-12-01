8e5e73945f9fb9342ccca48f46785c0b8f35eea6
SpikeHD <SpikeHD@users.noreply.github.com>
2024-12-01 01:10:48 +0000

    deploy: 3351a89c5648a40c4bd6d4a006ce040bddc67b03

--- shelteRPC/plugin.js
+++ shelteRPC/plugin.js
@@ -74,7 +74,6 @@
   // plugins/shelteRPC/index.tsx
   var shelteRPC_exports = {};
   __export(shelteRPC_exports, {
-    generateAssetId: () => generateAssetId,
     onLoad: () => onLoad,
     onUnload: () => onUnload,
     settings: () => settings
@@ -85,6 +84,153 @@
   var import_web25 = __toESM(require_web(), 1);
   var import_web26 = __toESM(require_web(), 1);
   var import_web27 = __toESM(require_web(), 1);
+
+  // node_modules/.pnpm/@cumjar+websmack@1.2.0/node_modules/@cumjar/websmack/src/raid/webpackChunk.js
+  var webpackChunk_default = (key) => {
+    key ??= Object.keys(window).find((key2) => key2.startsWith("webpackChunk"));
+    if (!window[key])
+      return;
+    let wpRequire;
+    window[key].push([
+      [Symbol()],
+      {},
+      (e) => {
+        wpRequire = e;
+      }
+    ]);
+    window[key].pop();
+    return [wpRequire.c ?? // wow thats jank lmao
+    Object.fromEntries(
+      Object.entries(wpRequire.m).map(([k]) => [
+        k,
+        { id: k, loaded: true, exports: wpRequire(k) }
+      ])
+    ), wpRequire];
+  };
+
+  // node_modules/.pnpm/@cumjar+websmack@1.2.0/node_modules/@cumjar/websmack/src/api/filters.js
+  var byProps = (props) => (m) => props.every((p) => m[p] !== void 0);
+  var byProtos = (protos) => (m) => m.prototype && protos.every((p) => m.prototype[p] !== void 0);
+  var byDisplayName = (name, defaultExp = true) => (m) => (defaultExp ? m.displayName : m.default?.displayName) === name;
+  var byKeyword = (strs) => (m) => strs.every(
+    (s) => Object.keys(m).some((k) => k.toLowerCase().includes(s.toLowerCase()))
+  );
+  var byDispNameDeep = (name) => (m) => {
+    const regex = new RegExp(`(${name}$)|((\\w+\\()+${name}\\))`);
+    if (regex.test(m.displayName))
+      return true;
+    if (typeof m.$$typeof !== "symbol")
+      return;
+    if (m.Consumer !== void 0)
+      return;
+    if (m.type || m.render) {
+      while (typeof m.type === "object" || typeof m.render === "object")
+        m = m.type ?? m.render;
+      if (regex.test(m.type?.displayName))
+        return true;
+      if (regex.test(m.render?.displayName))
+        return true;
+    }
+  };
+  var isKeyable = (m) => typeof m === "object" || typeof m === "function";
+  var byNestedProps = (props) => (m) => isKeyable(m) && Object.values(m).some(
+    (v) => isKeyable(v) && props.some((p) => v?.[p] !== void 0)
+  );
+  var allByCode = (modules, loaders) => (code) => Object.entries(loaders).filter(([, m]) => m.toString().match(code)).map(([id]) => modules[id]?.exports).filter((m) => m);
+
+  // node_modules/.pnpm/@cumjar+websmack@1.2.0/node_modules/@cumjar/websmack/src/api/batch.js
+  var batchFilter = (modules, filterList) => {
+    const found = [];
+    const checkModule = (mod) => filterList.forEach(([filter2, multi], i) => {
+      if (multi && !found[i])
+        found[i] = [];
+      if (filter2(mod)) {
+        if (multi)
+          found[i].push(mod);
+        else if (!found[i])
+          found[i] = mod;
+      }
+    });
+    for (const mid in modules) {
+      const module = modules[mid].exports;
+      if (!module || module === window)
+        continue;
+      if (module.default && module.__esModule)
+        checkModule(module.default);
+      checkModule(module);
+    }
+    return found;
+  };
+  var makeFakeWp = (filterList) => ({
+    find: (f) => filterList.push([f, false]),
+    findAll: (f) => filterList.push([f, true]),
+    findByProps: (...p) => filterList.push([byProps(p), false]),
+    findByPropsAll: (...p) => filterList.push([byProps(p), true]),
+    findByPrototypes: (...p) => filterList.push([byProtos(p), false]),
+    findByPrototypesAll: (...p) => filterList.push([byProtos(p), true]),
+    findByNestedProps: (...p) => filterList.push([byNestedProps(p), false]),
+    findByNestedPropsAll: (...p) => filterList.push([byNestedProps(p), true]),
+    findByDisplayName: (n, d) => filterList.push([byDisplayName(n, d), false]),
+    findByDisplayNameAll: (n, d) => filterList.push([byDisplayName(n, d), true]),
+    findByDispNameDeep: (n) => filterList.push([byDispNameDeep(n), false]),
+    findByDispNameDeepAll: (n) => filterList.push([byDispNameDeep(n), true]),
+    findByKeyword: (...s) => filterList.push([byKeyword(s), false]),
+    findByKeywordAll: (...s) => filterList.push([byKeyword(s), true])
+  });
+  var batch_default = (mods) => (cb) => {
+    const fList = [];
+    const fakeWp = makeFakeWp(fList);
+    cb(fakeWp);
+    return batchFilter(mods, fList);
+  };
+
+  // node_modules/.pnpm/@cumjar+websmack@1.2.0/node_modules/@cumjar/websmack/src/api/index.js
+  var filter = (modules, single = true) => (filterFunc) => {
+    const found = [];
+    for (const mid in modules) {
+      const module = modules[mid].exports;
+      if (!module || module === window)
+        continue;
+      if (module.default && module.__esModule && filterFunc(module.default)) {
+        if (single)
+          return module.default;
+        found.push(module.default);
+      }
+      if (filterFunc(module)) {
+        if (single)
+          return module;
+        found.push(module);
+      }
+    }
+    if (!single)
+      return found;
+  };
+  var api_default = ([, modules, wpR]) => {
+    const find = filter(modules);
+    const findAll = filter(modules, false);
+    const findByCodeAll = wpR ? allByCode(modules, wpR.m) : () => {
+      throw new Error("findByCode does not work with this bundler");
+    };
+    return {
+      batchFind: batch_default(modules),
+      find,
+      findAll,
+      findByProps: (...p) => find(byProps(p)),
+      findByPropsAll: (...p) => findAll(byProps(p)),
+      findByPrototypes: (...p) => find(byProtos(p)),
+      findByPrototypesAll: (...p) => findAll(byProtos(p)),
+      findByNestedProps: (...p) => find(byNestedProps(p)),
+      findByNestedPropsAll: (...p) => findAll(byNestedProps(p)),
+      findByDisplayName: (d, p) => find(byDisplayName(d, p)),
+      findByDisplayNameAll: (d, p) => findAll(byDisplayName(d, p)),
+      findByDispNameDeep: (d) => find(byDispNameDeep(d)),
+      findByDispNameDeepAll: (d) => findAll(byDispNameDeep(d)),
+      findByKeyword: (...k) => find(byKeyword(k)),
+      findByKeywordAll: (...k) => findAll(byKeyword(k)),
+      findByCodeAll,
+      findByCode: (c2) => findByCodeAll(c2)[0]
+    };
+  };
 
   // api/dorion.ts
   var dorion_default = {
@@ -690,6 +836,12 @@
     },
     http
   } = shelter;
+  var chunk = webpackChunk_default();
+  var wp = chunk && api_default([void 0, ...chunk]);
+  var c = wp.findByCode("getAssetImage: ");
+  var fetchAssetIds = (id, asset) => __async(void 0, null, function* () {
+    return (yield c.fetchAssetIds(id, [asset, null]))[0];
+  });
   var injectedCss4 = false;
   if (!injectedCss4) {
     injectedCss4 = true;
@@ -697,7 +849,6 @@
   }
   var maybeUnregisterGameSettings = [() => {
   }];
-  var cachedAssets = {};
   var ws;
   var apps = {};
   store3.currentlyPlaying = "";
@@ -707,19 +858,6 @@
       return ((_a = yield http.get(`/oauth2/applications/${appId}/rpc`)) == null ? void 0 : _a.body) || "Unknown";
     });
   }
-  var generateAssetId = (appId, asset) => __async(void 0, null, function* () {
-    var _a;
-    if (!cachedAssets[appId]) {
-      const resp = yield http.get(`/oauth2/applications/${appId}/assets`);
-      if (resp.status !== 200) {
-        console.log("Failed to fetch assets");
-      }
-      cachedAssets[appId] = resp.body;
-    }
-    const assetId = (_a = cachedAssets[appId].find((a) => a.name === asset)) == null ? void 0 : _a.id;
-    return assetId;
-  });
-  var isProbablyUrl = (str) => str.startsWith("http://") || str.startsWith("https://");
   function handleMessage(e) {
     return __async(this, null, function* () {
       var _a, _b, _c, _d, _e;
@@ -727,10 +865,10 @@
       const assets = (_a = data.activity) == null ? void 0 : _a.assets;
       if (data.cmd)
         return handleCmd(data);
-      if ((assets == null ? void 0 : assets.large_image) && !isProbablyUrl(assets.large_image))
-        assets.large_image = yield generateAssetId(data.activity.application_id, assets.large_image);
-      if ((assets == null ? void 0 : assets.small_image) && !isProbablyUrl(assets.small_image))
-        assets.small_image = yield generateAssetId(data.activity.application_id, assets.small_image);
+      if (assets == null ? void 0 : assets.large_image)
+        assets.large_image = yield fetchAssetIds(data.activity.application_id, assets.large_image);
+      if (assets == null ? void 0 : assets.small_image)
+        assets.small_image = yield fetchAssetIds(data.activity.application_id, assets.small_image);
       if (data.activity) {
         const appId = data.activity.application_id;
         apps[appId] || (apps[appId] = yield lookupApp(appId).catch(() => "Unknown"));
