2f9d9c9d9785fcad7d93e993de98085f1d985612
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-06-12 20:07:12 -0700

    fix: selectors

--- plugins/dorion-titlebar/actions.ts
+++ plugins/dorion-titlebar/actions.ts
@@ -1,3 +1,5 @@
+import { classes } from './index.scss'
+
 export function close() {
   // @ts-expect-error shut up
   window.__TAURI__.core.invoke('close')
@@ -16,10 +18,10 @@ export function toggleMaximize() {
 export async function setMaximizeIcon() {
   // @ts-expect-error shut up
   if (await window.__TAURI__.webviewWindow.getCurrentWebviewWindow().isMaximized()) {
-    const topmax = document.querySelector('#topmax') as HTMLDivElement
-    topmax.classList.add('maximized')
+    const topmax = document.querySelector(`.${classes.topmax}`) as HTMLDivElement
+    topmax.classList.add(classes.maximized)
   } else {
-    const topmax = document.querySelector('#topmax') as HTMLDivElement
-    topmax.classList.remove('maximized')
+    const topmax = document.querySelector(`.${classes.topmax}`) as HTMLDivElement
+    topmax.classList.remove(classes.maximized)
   }
 }
