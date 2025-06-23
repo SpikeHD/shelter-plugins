743ed62d01a6e159691a3c9a8f13cc5cae46eb8f
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-06-23 16:58:19 -0700

    fix(declutter): start with all enabled

--- plugins/clean-home/index.tsx
+++ plugins/clean-home/index.tsx
@@ -76,6 +76,20 @@ const setStyle = () => {
 // Initial call when we load
 setStyle()
 
+// If this is the first time it's ever been loaded, enable everything and send a toast
+if (Object.keys(store).length === 0) {
+  components.forEach(c => {
+    store[c.name] = true
+  })
+  setStyle()
+
+  shelter.ui.showToast({
+    title: 'Declutter',
+    content: 'All component removals have been enabled. Click the settings icon to disable them selectively.',
+    duration: 3000,
+  })
+}
+
 export const settings = () => {
   return (
     <div style={{ height: '50vh', overflow: 'auto' }}>
